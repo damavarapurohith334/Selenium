85e2540c1d6ec140cd4dca293d591e563ed3e03e
damavarapurohith334 <125539223+damavarapurohith334@users.noreply.github.com>
2025-07-09 01:46:00 +0530

    Update BaseTest.java "removed extras"

--- src/base/BaseTest.java
+++ src/base/BaseTest.java
@@ -6,10 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-
 import utils.ExtentReportManager;
 
 public class BaseTest {
